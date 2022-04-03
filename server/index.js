6ed4e8efa4cae11465794735e2bfb6ca4f573f67
Nisal Renuja Palliyaguru <42801983+nisalrenuja@users.noreply.github.com>
2022-04-04 00:41:22 +0530

    Cors added

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
-app.get("/hello", (req, res) => {
-  res.send("Hello World");
+app.use(cors());
+app.use(express.json());
+
+app.post("/api/register", (req, res) => {
+  console.log(req.body);
+  res.json({ status: "ok" });
 });
 
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+app.listen(8080, () => {
+  console.log("Server is running on port 8080");
 });
