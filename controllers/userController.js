83298132f579aa45d2f55d47d8d838f3e0f5d7a4
Nisal Renuja Palliyaguru <42801983+nisalrenuja@users.noreply.github.com>
2022-04-09 04:12:36 +0530

    Forgot password feature added

--- controllers/userController.js
+++ controllers/userController.js
@@ -134,6 +134,55 @@ const userController = {
       return res.status(500).json({ msg: err.message });
     }
   },
+  forgot: async (req, res) => {
+    try {
+      // get email
+      const { email } = req.body;
+
+      // check email
+      const user = await User.findOne({ email });
+      if (!user)
+        return res
+          .status(400)
+          .json({ msg: "This email is not registered in our system." });
+
+      // create ac token
+      const ac_token = createToken.access({ id: user.id });
+
+      // send email
+      const url = `http://localhost:3000/auth/reset-password/${ac_token}`;
+      const name = user.name;
+      sendMail.sendEmailReset(email, url, "Reset your password", name);
+
+      // success
+      res
+        .status(200)
+        .json({ msg: "Re-send the password, please check your email." });
+    } catch (err) {
+      res.status(500).json({ msg: err.message });
+    }
+  },
+  reset: async (req, res) => {
+    try {
+      // get password
+      const { password } = req.body;
+
+      // hash password
+      const salt = await bcrypt.genSalt();
+      const hashPassword = await bcrypt.hash(password, salt);
+
+      // update password
+      await User.findOneAndUpdate(
+        { _id: req.user.id },
+        { password: hashPassword }
+      );
+
+      // reset success
+      res.status(200).json({ msg: "Password was updated successfully." });
+    } catch (err) {
+      res.status(500).json({ msg: err.message });
+    }
+  },
 };
 
 module.exports = userController;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,5 +7,7 @@ route.post("/api/auth/register", userController.register);
 route.post("/api/auth/activation", userController.activate);
 route.post("/api/auth/signing", userController.signing);
 route.post("/api/auth/access", userController.access);
+route.post("/api/auth/forgot_pass", userController.forgot);
+route.post("/api/auth/reset_pass", auth, userController.reset);
 
 module.exports = route;
