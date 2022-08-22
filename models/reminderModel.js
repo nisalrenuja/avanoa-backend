5eb5d5f117b4e3734e561de8078f3faa272124a9
nuduja <nudujabandara@gmail.com>
2022-08-22 22:45:45 +0530

    Reminder Model added

---
+++ models/reminderModel.js
@@ -0,0 +1,28 @@
+const { Schema, model } = require("mongoose");
+
+const reminderSchema = new Schema(
+  {
+    Title: {
+      type: String,
+      required: [true, "Please enter the Title"],
+      trim: true,
+    },
+    Description: {
+        type: String,
+        required: [true, "Please enter the Description"],     
+        trim: true,
+        unique: true, 
+    },
+    Time: {
+        type: String,
+        required: [true, "Please enter your email"],
+        trim: true,
+        unique: true,
+    },    
+  },
+  { timestamp: true }
+);
+
+const Reminder = model("Reminder", reminderSchema);
+
+module.exports = Reminder;
