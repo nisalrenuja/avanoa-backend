0ffe650bb8f0a5eb975d0be75b1c4be8133adcc6
Nisal Renuja Palliyaguru <42801983+nisalrenuja@users.noreply.github.com>
2022-10-22 22:54:08 +0530

    models and routes updated

--- models/emailModel.js
+++ models/emailModel.js
@@ -6,16 +6,22 @@ const emailSchema = new Schema(
       type: String,
       required: [true, "Please enter the email1"],
       trim: true,
+      default: "nrenuja@gmail.com",    
+      unique: false,
     },
     email2: {
       type: String,
       required: [true, "Please enter the email2"],
       trim: true,
+      default: "nrenuja@gmail.com",
+      unique: false, 
     },
     email3: {
       type: String,
       required: [true, "Please enter the email3"],
       trim: true,
+      default: "nrenuja@gmail.com", 
+      unique: false,
     },
   },
   { timestamp: true }
--- routes/emailRoutes.js
+++ routes/emailRoutes.js
@@ -3,7 +3,37 @@ const Email = require("../models/emailModel");
 
 const router = express.Router();
 
-router.patch("/email/update/:id", (req, res) => {
+router.post("/emails/save", (req, res) => {
+  let newEmail = new Email(req.body);
+
+  newEmail.save((err) => {
+    if (err) {
+      return res.status(400).json({
+        error: err,
+      });
+    }
+    return res.status(200).json({
+      success: "Email saved successfully",
+      id: newEmail._id,
+    });
+  });
+});
+
+router.get("/emails", (req, res) => {
+  Email.find().exec((err, emails) => {
+    if (err) {
+      return res.status(400).json({
+        error: err,
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      existingEmails: emails,
+    });
+  });
+});
+
+router.patch("/emails/update/:id", (req, res) => {
   Email.findByIdAndUpdate(
     req.params.id,
     {
@@ -20,5 +50,19 @@ router.patch("/email/update/:id", (req, res) => {
     }
   );
 });
+
+router.get("/emails", (req, res) => {
+  Email.find().exec((err, email) => {
+    if (err) {
+      return res.status(400).json({ error: err });
+    }
+
+    return res.status(200).json({
+      success: true,
+      existingEmail: email,
+    });
+  });
+});
+
 
 module.exports = router;
